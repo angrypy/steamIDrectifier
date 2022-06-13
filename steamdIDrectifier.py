9f5d210f8f9a2a71227ffa032c92115d7e28d533
angrypy <wopker11@gmail.com>
2022-06-13 16:37:49 +0300

    small fix 4

--- steamdIDrectifier.py
+++ steamdIDrectifier.py
@@ -1,20 +1,26 @@
 # Example:
 # https://steamcommunity.com/id/ppaphere1488
 # https://steamcommunity.com/profiles/76561198016174618
+# <div id="steamnameblock"><span id="steamname" title="PPAP" class="steamname ">PPAP</span></div>
+
 
 import requests
 import re
 
 inputLinks = input('Введите ссылку для очищения: ')
 r = requests.get(f'http://steamrep.com/search?q={inputLinks}')
+print('r: ', r)
+steamIDPattern = r'(?<=|)(\d{17})(?=|)'
+steamNickname = r'(?<= ">)(.*)(?=</span>)'
+steamIDP = re.search(steamIDPattern, r.text)
+steamNickP = re.search(steamNickname, r.text)
+print('nickName: ', steamNickP)
+if steamIDP != None:
+    steamID = steamIDP.group(0)
+    nick = steamNickP.group(0)
 
-SteamIDPattern = r'(?<=|)(\d{17})(?=|)'
-SteamIDP = re.search(SteamIDPattern, r.text)
-if SteamIDP != None:
-    a = SteamIDP.group(0)
-
-with open(f'fileDone_{a}.txt', 'w', encoding='utf-8') as myDoneTXT:
-    myDoneTXT.write(f'https://steamcommunity.com/profiles/{a}')
+with open(f'fileDone_{steamID}.txt', 'w', encoding='utf-8') as myDoneTXT:
+    myDoneTXT.write(f'https://steamcommunity.com/profiles/{steamID}\n')
+    myDoneTXT.write(f'{steamID} "{nick}"')
 
 input('Программа выполнила работу, нажмите Enter для выхода...')
-
