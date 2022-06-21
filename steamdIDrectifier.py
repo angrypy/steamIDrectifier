e67e6f61d1d882f17a94b26264927f1dedd1fb35
angrypy <wopker11@gmail.com>
2022-06-21 18:21:00 +0300

    new parser fix small bug

--- steamdIDrectifier.py
+++ steamdIDrectifier.py
@@ -1,20 +1,18 @@
 # Example:
 # https://steamcommunity.com/id/ppaphere1488
 # https://steamcommunity.com/profiles/76561198016174618
-# <div id="steamnameblock"><span id="steamname" title="PPAP" class="steamname ">PPAP</span></div>
 
 
 import requests
 import re
 
 inputLinks = input('Введите ссылку для очищения: ')
-r = requests.get(f'http://steamrep.com/search?q={inputLinks}')
-print('r: ', r)
-steamIDPattern = r'(?<=|)(\d{17})(?=|)'
-steamNickname = r'(?<= ">)(.*)(?=</span>)'
+r = requests.get(inputLinks)
+steamIDPattern = r'(?<="steamid":")(.+)(?=","personaname)'
+steamNickname = r'(?<="personaname":")(.+)(?=","summary)'
 steamIDP = re.search(steamIDPattern, r.text)
 steamNickP = re.search(steamNickname, r.text)
-print('nickName: ', steamNickP)
+
 if steamIDP != None:
     steamID = steamIDP.group(0)
     nick = steamNickP.group(0)
